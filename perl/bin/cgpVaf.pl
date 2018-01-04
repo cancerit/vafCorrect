a772a41528757020cc984840b322c37c58d14e8d
Shriram Bhosle <sb43@sanger.ac.uk>
2018-01-04 16:37:49 +0000

    updated version and CHANGES.md

--- perl/bin/cgpVaf.pl
+++ perl/bin/cgpVaf.pl
@@ -203,7 +203,7 @@ sub option_builder {
 	pod2usage(q{'-a' variant type must be defined}) unless (defined $options{'a'});
 	pod2usage(q{'-tn' toumour sample name/s must be provided}) unless (defined $options{'tn'});
 	pod2usage(q{'-nn' normal sample name/s must be provided}) unless (defined $options{'nn'});
-  pod2usage(q{'-e' Input vcf file extension must be provided}) unless (defined $options{'e'});
+  pod2usage(q{'-e' Input vcf file extension must be provided}) unless (defined $options{'bo'});
 	pod2usage(q{'-b' bed file must be specified }) unless (defined $options{'b'} || defined $options{'e'});
   pod2usage(q{'-o' Output folder must be provided}) unless (defined $options{'o'});
 
@@ -289,7 +289,7 @@ cgpVaf.pl [-h] -d -a -g -tn -nn -e  -o [ -b -t -c -r -m -ao -mq -pid -bo -vcf -v
    --tumour_name    (-tn)  Toumour sample name [ list of space separated  sample names ]
    --normal_name    (-nn)  Normal sample name [ single sample used as normal for this analysis ]
    --outDir         (-o)   Output folder
-   --vcfExtension   (-e)   vcf file extension string after the sample name - INCLUDE's preceding dot (default: .caveman_c.annot.vcf.gz)
+   --vcfExtension   (-e)   vcf file extension string after the sample name - INCLUDE's preceding dot (default: .caveman_c.annot.vcf.gz) [ optional if -bo 1 ]
 
   Optional
    --infoTags       (-t)   comma separated list of tags to be included in the tsv output, vcf file by default includes all data
